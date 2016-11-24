2ee4ca20049a494ce1b3e258d341af987141f514
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-11-24 20:45:03 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate num_complex;
 
 use num_complex::{Complex, Complex64};
 
-fn compute_pixel(x: i32, y: i32, w: i32, h: i32, iterations: u32) -> Complex64 {
+fn compute_pixel(x: u32, y: u32, w: u32, h: u32, iterations: u32) -> Complex64 {
     let normalized_x = x as f64 * (3.0 / w as f64) - 2.0;
     let normalized_y = y as f64 * (2.0 / h as f64) - 1.0;
 
@@ -23,14 +23,17 @@ fn init_image() -> () {
 fn write_image() {}
 
 fn main() {
-    let mut img = ImageBuffer::new(512, 512);
+    const W: u32 = 800;
+    const H: u32 = 600;
+
+    let mut img = ImageBuffer::new(W, H);
     img.put_pixel(1, 1, image::Luma([255u8]));
     img.save("img.png");
-    const W: i32 = 800;
-    const H: i32 = 600;
 
     let iterations = 5;
     let image = init_image();
+
+    println!("{:?}", compute_pixel(5, 5, 100, 100, 5));
 
     for x in 0..W {
         for y in 0..H {
