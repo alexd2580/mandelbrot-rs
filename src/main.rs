fd6d517fc23991387dcd7e4baf0c23bd8e53d0b6
Vitalii Lysov <vitalii.lysov@gmail.com>
2016-11-24 20:31:32 +0100

    implementation of compute_pixel

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ extern crate num_complex;
 use num_complex::{Complex, Complex64};
 
 fn compute_pixel(x: i32, y: i32, w: i32, h: i32, iterations: u32) -> Complex64 {
-    
-    Complex::new(0.0, 0.0)
+    let normalized_x = x as f64 * (3.0 / w as f64) - 2.0;
+    let normalized_y = y as f64 * (2.0 / h as f64) - 1.0;
+
+    if iterations > 0 {
+        let previous_z = compute_pixel(x, y, w, h, iterations - 1);
+        &previous_z * &previous_z + Complex::new(normalized_x, normalized_y)
+    } else {
+        Complex::new(0.0, 0.0)
+    }
 }
 
 fn init_image() -> () {
@@ -19,15 +26,15 @@ fn main() {
     let mut img = ImageBuffer::new(512, 512);
     img.put_pixel(1, 1, image::Luma([255u8]));
     img.save("img.png");
-    const w: i32 = 800;
-    const h: i32 = 600;
+    const W: i32 = 800;
+    const H: i32 = 600;
 
     let iterations = 5;
     let image = init_image();
 
-    for x in 0..w {
-        for y in 0..h {
-            let pixel = compute_pixel(x, y, w, h, iterations);
+    for x in 0..W {
+        for y in 0..H {
+            let pixel = compute_pixel(x, y, W, H, iterations);
         }
     }
 
