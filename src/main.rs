5d6ca799f1287b7753749bead6762441ed2bd66c
Hans Ole Hatzel <hhatzel@gmail.com>
2016-11-24 19:57:34 +0100

    Use image library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate image;
+use image::ImageBuffer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = ImageBuffer::new(512, 512);
+    img.put_pixel(1, 1, image::Luma([255u8]));
+    img.save("img.png");
 }
