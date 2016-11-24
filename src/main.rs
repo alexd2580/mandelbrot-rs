ce0009bb16f5ebf6332fd4207ad8b8c2d10e0a2b
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-11-24 19:57:16 +0100

    Add skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate num_complex;
+
+use num_complex::{Complex, Complex64};
+
+fn compute_pixel(x: i32, y: i32, w: i32, h: i32, iterations: u32) -> Complex64 {
+    Complex::new(0.0, 0.0)
+}
+
+fn init_image() -> () {
+    ()
+}
+
+fn write_image() {}
+
 fn main() {
-    println!("Hello, world!");
+    const w: i32 = 800;
+    const h: i32 = 600;
+
+    let iterations = 5;
+    let image = init_image();
+
+    for x in 0..w {
+        for y in 0..h {
+            let pixel = compute_pixel(x, y, w, h, iterations);
+        }
+    }
+
+    write_image();
 }
