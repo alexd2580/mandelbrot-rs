ba88126a97b97fc3b4ddc14341ba7a2ceacf7749
Hans Ole Hatzel <hhatzel@gmail.com>
2016-11-24 20:41:27 +0100

    Function for generation fractal.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate image;
-use image::ImageBuffer;
 extern crate num_complex;
+use image::ImageBuffer;
 
 use num_complex::{Complex, Complex64};
 
@@ -15,20 +15,36 @@ fn init_image() -> () {
 fn write_image() {}
 
 fn main() {
-    let mut img = ImageBuffer::new(512, 512);
-    img.put_pixel(1, 1, image::Luma([255u8]));
-    img.save("img.png");
-    const w: i32 = 800;
-    const h: i32 = 600;
-
+    const w: u32 = 800;
+    const h: u32 = 600;
     let iterations = 5;
+    let d = generate_fractal(w, h, iterations);
     let image = init_image();
+    //let mut img = ImageBuffer::new(w, h);
+    
+    //img.save("img.png");
+}
 
+fn generate_fractal(w: u32, h: u32, iterations: u32) -> Box<[u8]> {
+    let mut vec_data = vec!(0u8; (w * h) as usize);
+    let mut data = vec_data.into_boxed_slice();
     for x in 0..w {
         for y in 0..h {
-            let pixel = compute_pixel(x, y, w, h, iterations);
+            let pixel = compute_pixel(x as i32, y as i32, w as i32, h as i32, iterations);
+            data[(y * w + x) as usize] = 0;
         }
+    };
+    data
+}
+
+mod tests {
+    #[bench]
+    fn benchmark_arc(b: &mut Bencher) {
+        b.iter(|| generate_fractal_arc());
     }
 
-    write_image();
+    #[bench]
+    fn benchmark_unsafe(b: &mut Bencher) {
+        b.iter(|| generate_fractal_unsafe());
+    }
 }
