39ecf1faf87f53008b7d9e319159c811b41e964b
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-11-25 01:03:15 +0100

    WIP: Make the renderer multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate image;
 extern crate num_complex;
 use image::ImageBuffer;
-
 use num_complex::{Complex, Complex64};
+use std::thread;
 
 fn compute_pixel(x: u32, y: u32, w: u32, h: u32, iterations: u32) -> Complex64 {
     let normalized_x = x as f64 * (3.0 / w as f64) - 2.0;
@@ -17,13 +17,11 @@ fn compute_pixel(x: u32, y: u32, w: u32, h: u32, iterations: u32) -> Complex64 {
 }
 
 fn main() {
-    const W: u32 = 800;
-    const H: u32 = 600;
-    let iterations = 5;
+    const W: u32 = 4096;
+    const H: u32 = 2160;
+    let iterations = 100;
     let d = generate_fractal(W, H, iterations);
-    let mut img = ImageBuffer::from_fn(W, H, |x, y| {
-        image::Luma([d[(y * W + x) as usize]])
-    });
+    let mut img = ImageBuffer::from_fn(W, H, |x, y| image::Luma([d[(y * W + x) as usize]]));
 
     img.save("img.png");
 }
@@ -31,10 +29,19 @@ fn main() {
 fn generate_fractal(W: u32, H: u32, iterations: u32) -> Box<[u8]> {
     let mut vec_data = vec!(0u8; (W * H) as usize);
     let mut data = vec_data.into_boxed_slice();
-    for x in 0..W {
-        for y in 0..H {
-            let comp = compute_pixel(x, y, W, H, iterations);
-            data[(y * W + x) as usize] = (comp.norm().ln() * 255.0) as u8;
+    let mut threads = Vec::new();
+    unsafe {
+        for x in 0..W {
+            threads.push(thread::spawn(move || {
+                for y in 0..H {
+                    let comp = compute_pixel(x, y, W, H, iterations);
+                    data[(y * W + x) as usize] = (comp.norm().ln() * 255.0) as u8;
+                }
+            }));
+        }
+
+        for thread in threads {
+            thread.join();
         }
     }
     data
